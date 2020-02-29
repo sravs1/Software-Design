d1080d15d7585236ee7dfc1e18528904ac2dfd66
SRAVANTHI PASUMARTHI <sravsp001@gmail.com>
2020-02-29 14:39:01 -0600

    Add files via upload

---
+++ valid.js
@@ -0,0 +1,49 @@
+	//Validtion Code For Inputs
+
+var email = document.forms['form']['email'];
+console.log(email);
+var password = document.forms['form']['password'];
+
+var email_error = document.getElementById('email_error');
+var pass_error = document.getElementById('pass_error');
+
+email.addEventListener('textInput', email_Verify);
+password.addEventListener('textInput', pass_Verify);
+
+function validated(){
+	if (email.value.length == 0 && password.value.length == 0 ) {
+		email.style.border = "1px solid red";
+		email_error.style.display = "block";
+		email.focus();
+		password.style.border = "1px solid red";
+		pass_error.style.display = "block";
+		password.focus();
+		return false;
+	}
+	if (email.value.length == 0) {
+		email.style.border = "1px solid red";
+		email_error.style.display = "block";
+		email.focus();
+		return false;
+	}
+	if (password.value.length == 0){
+	password.style.border = "1px solid red";
+	pass_error.style.display = "block";
+	password.focus();
+	return false;
+}
+}
+function email_Verify(){
+	if (email.value.length >= 8) {
+		email.style.border = "1px solid silver";
+		email_error.style.display = "none";
+		return true;
+	}
+}
+function pass_Verify(){
+	if (password.value.length >= 5) {
+		password.style.border = "1px solid silver";
+		pass_error.style.display = "none";
+		return true;
+	}
+}
